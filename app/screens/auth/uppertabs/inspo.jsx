b90f5847c4638b1b294fac6ac10172ae55865305
FabiTosa <9022279@student.zadkine.nl>
2025-03-10 13:40:23 +0100

    .

---
+++ app/screens/auth/uppertabs/inspo.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function inspo() {
+  return (
+    <View>
+      <Text>inspo</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ app/screens/auth/uppertabs/newarrivals.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function newarrivals() {
+  return (
+    <View>
+      <Text>newarrivals</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
