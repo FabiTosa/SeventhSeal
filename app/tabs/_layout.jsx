4a9e8ac0e684fb98992aaea1a44fc73698474537
FabiTosa <9022279@student.zadkine.nl>
2025-03-11 13:05:49 +0100

    navigation

--- app/tabs/_layout.jsx
+++ app/tabs/_layout.jsx
@@ -1,12 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Ionicons } from "@expo/vector-icons"
+import { Tabs } from "expo-router"
 
-export default function _layout() {
+
+export default function TabsLayout() {
   return (
-    <View>
-      <Text>_layout</Text>
-    </View>
+    <Tabs screenOptions={{ headerShown: false }}>
+
+      <Tabs.Screen name="search" options={{ title: "", tabBarIcon: () => <Ionicons name='create-outline' size='24' />, }} />
+
+      <Tabs.Screen name="bag" options={{ title: "", tabBarIcon: () => <Ionicons name='search-outline' size='24' />, }} />
+
+      <Tabs.Screen name="home" options={{ title: "", tabBarIcon: () => <Ionicons name='home-outline' size='24' />, }} />
+
+      <Tabs.Screen name="profile" options={{ title: "", tabBarIcon: () => <Ionicons name='person-outline' size='24' />, }} />
+
+    </Tabs>
   )
 }
-
-const styles = StyleSheet.create({})
