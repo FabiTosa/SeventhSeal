0fed11ab67b5530bd9d7c3be271def3338eeef3b
FabiTosa <9022279@student.zadkine.nl>
2025-04-07 13:45:56 +0200

    gf

--- app/index.jsx
+++ app/index.jsx
@@ -1,29 +1,4 @@
-import { useRouter } from 'expo-router'
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 
-
-
-export default function Index() {
-  
-  const router = useRouter()
-
-  return (
-    <View style={styles.view}>
-      <TouchableOpacity style={styles.button} onPress={() => router.replace("/screens/tabs/home")}>
-        <Text style={styles.buttonText}>GO TO THE APP</Text>
-      </TouchableOpacity>
-    </View>
-  )
-}
-
-const styles = StyleSheet.create({
-  view: {
-      flex: 1,
-      backgroundColor: 'white',
-      justifyContent: "center",
-      alignItems: "center",
-  },
-})
 import { View, Text, StyleSheet, Pressable, ImageBackground } from 'react-native';
 import { useRouter } from 'expo-router';
 
